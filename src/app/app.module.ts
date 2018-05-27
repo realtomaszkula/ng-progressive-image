271a81abd2335d4e1c50d0b2a8af57d0a48803c5
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-05-27 23:02:31 +0200

    feat: add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule
+} from '@angular/material';
 import { AppComponent } from './app.component';
 import { ProgressiveImageModule } from './progressive-image/progressive-image.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, ProgressiveImageModule],
+  imports: [
+    BrowserModule,
+    ProgressiveImageModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
