c5278f9b84798adc8912b734ce6277d34585beb3
Piotr Kula <piotrkulawebdev@gmail.com>
2019-10-26 20:44:49 +0200

    update to ng8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatButtonModule,
-  MatIconModule,
-  MatToolbarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { ProgressiveImageModule } from './progressive-image/progressive-image.module';
 import { ToolbarComponent } from './layout/toolbar.component';
